708e247e80963452a8c73054b030e39400186d36
Sayok Bhattacharya <namesake88@gmail.com>
2016-06-13 17:53:41 +0530

    added project twift and changed db to postgres

--- projectx/settings.py
+++ projectx/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'projectx.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'projectxdb',
+          'USER': 'projectxadmin',
+          'PASSWORD':'projectx@123',
+          'HOST': 'localhost',
+          'PORT': '5432',
+      }
+  }
 
 
 # Password validation
