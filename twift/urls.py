9571705ff50c9a1e03882861f66acb337b44a20c
JkShaw <jyotishshaw33@gmail.com>
2016-06-29 14:00:48 +0530

    Offer-ride page UI ready

--- twift/urls.py
+++ twift/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic.base import TemplateView
 
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/', views.home, name='home'),
+    url(r'^thanks/', TemplateView.as_view(template_name='twift/thanks.html'),
+        name='thanks'),
+    url(r'^offerRide/',
+        TemplateView.as_view(template_name='twift/offerRide.html'),
+        name='offerRide'),
 ]
--- twift/views.py
+++ twift/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 
 def home(request):
     return render(request, "home.html")
+
+
+def thanks(request):
+    if request.method == 'POST':
+        return render(request, "twift/offerRide.html")
+
+def ride_offer(request):
+    if request.method == 'POST':
+        form = RideForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = RideForm()
+
+    return render(request, "twift/offerRide.html", {'form': form})
